675312e249f2206904bb538036bbeb87e563e979
Dean Howe <hideanhowe@gmail.com>
2026-02-11 11:49:23 +0000

    solution

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ function findPawnMoves(position) {
   console.log(typeof position);
 
   // check the position number
-  const number = Number.parseInt(position[1]);
+  const number = parseInt(position[1]);
   console.log(number);
 
   // if position is on starting row
-  let letter = position[0];
+  const letter = position[0];
 
-  if (number == !1) {
-    return `${letter + (number + 1)}`;
-  } else {
-    return `[${letter + (number + 1)}, ${letter + (number + 2)}]`;
+  // arr for moves
+  const moves = [];
+
+  // top of board
+  if (number === 8) return [];
+
+  // Standard move, one square up;
+  moves.push(`${letter + (number + 1)}`);
+
+  if (number === 2) {
+    moves.push(`${letter + (number + 2)}`);
   }
+
+  return moves;
 
   // return position;
 }
